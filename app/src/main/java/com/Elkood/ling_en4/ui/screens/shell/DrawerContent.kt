aea18cee0784d173edc2becad60fbe8ea2408402
abed-supy-io <abed@supy.io>
2026-07-28 19:42:41 +0400

    refactor: drop unused imports from DrawerContent

--- app/src/main/java/com/Elkood/ling_en4/ui/screens/shell/DrawerContent.kt
+++ app/src/main/java/com/Elkood/ling_en4/ui/screens/shell/DrawerContent.kt
@@ -1,7 +1,5 @@
 package com.Elkood.ling_en4.ui.screens.shell
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
